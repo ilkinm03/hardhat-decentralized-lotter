784a73df1f915255b16c4d447a55cc58f5550f59
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2024-04-14 12:48:20 +0400

    feat: implement enter lottery method

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,13 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+    error Lottery_InsufficientFunds();
+
 contract Lottery {
 
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
+    event LotteryEnter(address indexed player);
+
+    modifier requireMinimumValue() {
+        if (msg.value < i_entranceFee) {
+            revert Lottery_InsufficientFunds();
+        }
+        _;
+    }
+
     constructor(uint256 _entranceFee) {
         i_entranceFee = _entranceFee;
+    }
+
+    function enterLottery() public payable requireMinimumValue {
+        s_players.push(payable(msg.sender));
+        emit LotteryEnter(msg.sender);
     }
 
     function getEntranceFee() public view returns (uint256) {
